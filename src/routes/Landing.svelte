2c772eea740ce83cc3dd6b8ee22eb2810fd50141
Koy Wilson <koywilson@Koys-MacBook-Air.local>
2024-02-10 17:52:21 -0600

    Added custom fonts

--- src/routes/Landing.svelte
+++ src/routes/Landing.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import { Link } from "svelte-routing";
+  import "../assets/fonts/streamster/fonts.css";
 </script>
 
 <main>Welcome to Internet of Pings</main>
 
 <Link to="/sim">Start</Link>
+
+<style>
+  main {
+    font-family: 'Streamster';
+    font-size: 80px;
+    color: #f890e7;
+  }
+</style>
