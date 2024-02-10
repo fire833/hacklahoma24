3c1d6c28192e46ebab397cd6cf577a008981b9a0
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-10 12:15:16 -0600

    Added Landing and Sim components

---
+++ src/routes/Landing.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { Link } from "svelte-routing";
+</script>
+
+<main>Welcome to Internet of Pings</main>
+
+<Link to="/sim">Blog</Link>
---
+++ src/routes/Sim.svelte
@@ -0,0 +1,1 @@
+<main>Simulator</main>
