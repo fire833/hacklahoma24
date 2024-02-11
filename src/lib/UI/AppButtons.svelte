f4f4ac97a41454a7bb446259df9bb32784ac838d
dcarse17 <dcarse17@gmail.com>
2024-02-11 06:26:50 -0600

    Adding functionality to tutorials

--- src/lib/UI/AppButtons.svelte
+++ src/lib/UI/AppButtons.svelte
@@ -2,12 +2,36 @@
   import Car from "../../assets/sprites/Car.png";
   import Computer from "../../assets/sprites/Computer.png";
   import Router from "../../assets/sprites/Router.png";
+  import { onMount } from 'svelte';
+  import tutorialData1 from "./Tutorial_1.json";
+  import tutorialData2 from "./Tutorial_2.json";
+  import { Link } from "svelte-routing";
   let current = "";
+  let counter = 1;
   let applications = [
     { name: "Ping", logo: Car },
     { name: "YouTube", logo: Computer },
     { name: "Netflix", logo: Router },
   ];
+
+
+  const urlParam = new URLSearchParams(window.location.search).get("level");
+
+  let currentTut = tutorialData1;
+
+  function forwardClick() {
+    event?.preventDefault();
+    counter = counter + 1;
+    currentTut = eval(`tutorialData${counter}`);
+  }
+
+  function backwardClick() {
+    event?.preventDefault();
+    counter = counter - 1;
+    currentTut = eval(`tutorialData${counter}`);
+  }
+
+
 </script>
 
 {#each applications as { name, logo } (logo)}
@@ -19,6 +43,35 @@
     {name}
   </button>
 {/each}
+
+
+{#if currentTut}
+<h1>{currentTut.title}</h1>
+
+{#each currentTut.sections as section (section.title)}
+  <div>
+    <h2>{section.title}</h2>
+    {#each section.steps as step (step.text)}
+      <p>{step.text}</p>
+    {/each}
+  </div>
+{/each}
+{/if}
+
+{#if counter >= 2}
+<a href="sim?level=tutorial_{counter}"on:click={backwardClick}>Back</a>
+
+{/if}
+
+{#if counter <= 6} 
+<a href="sim?level=tutorial_{counter}" on:click={forwardClick}>Next</a>
+
+{/if}
+
+
+ <!-- button on click do this and then get new url at same time so it updates -->
+<Link to="/sim?level=tutorial_2">Start</Link>
+
 
 <style>
   .app {
