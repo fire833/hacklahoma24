3fcadf7fadbf51abb0b73a8a315703bca52135ee
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-11 03:43:25 -0600

    Started VisNode object

--- src/lib/canvas/visObjects.ts
+++ src/lib/canvas/visObjects.ts
@@ -1,7 +1,17 @@
+import type { Sprite } from "pixi.js";
 import type { Network, Node } from "../../net/net.js";
 
 export class VisNode {
   node: Node | undefined;
+  sprite: Sprite | undefined;
+
+  constructor(node: Node) {
+    this.node = node;
+    this.sprite = this.makeSprite();
+  }
+
+  // Creates PIXI Sprite object and returns it
+  makeSprite(): Sprite {}
 }
 
 export class VisEdge {
