75d840c8b15800072f8dc72d0535570099210691
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-10 22:16:14 -0600

    Added viewport

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
   import { afterUpdate, onMount } from "svelte";
   import { addStarterObjects, placeCanvas } from "./canvas/placeCanvas.js";
-  import { speed, visMode } from "../stores";
   import type { Network } from "../net/net.js";
   import type { Application } from "pixi.js";
+  import type { Viewport } from "pixi-viewport";
 
   export let net: Network;
 
   let canvas: HTMLElement;
 
   let app: Application<HTMLCanvasElement>;
+  let viewport: Viewport;
 
   onMount(async () => {
-    app = placeCanvas(canvas);
-    addStarterObjects(app, net, $speed, $visMode);
+    let c = placeCanvas(canvas);
+    app = c[0];
+    viewport = c[1];
+    addStarterObjects(app, net);
   });
 </script>
 
--- src/lib/canvas/placeCanvas.ts
+++ src/lib/canvas/placeCanvas.ts
@@ -1,93 +1,91 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
+import { Viewport } from "pixi-viewport";
 import Envelope from "../../assets/sprites/Envelope.png";
 import Router from "../../assets/sprites/Router.png";
 import { VisMode, visMode, speed } from "../../stores";
 import type { Network } from "../../net/net.js";
 
-export function placeCanvas(canvas: HTMLElement) {
-    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-    // The application will create a renderer using WebGL, if possible,
-    // with a fallback to a canvas render. It will also setup the ticker
-    // and the root stage PIXI.Container
-    const app = new PIXI.Application<HTMLCanvasElement>({backgroundAlpha: 0});
+export let viewport: Viewport;
 
-    // Function to handle resize event
-    function resize() {
-      app.renderer.resize(window.innerWidth, window.innerHeight);
-    }
+export function placeCanvas(
+  canvas: HTMLElement
+): [PIXI.Application<HTMLCanvasElement>, Viewport] {
+  PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST;
+  // The application will create a renderer using WebGL, if possible,
+  // with a fallback to a canvas render. It will also setup the ticker
+  // and the root stage PIXI.Container
+  const app = new PIXI.Application<HTMLCanvasElement>({
+    backgroundAlpha: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: true,
+    autoDensity: true,
+  });
 
-    // Listen for window resize events
-    window.addEventListener("resize", resize);
+  // The application will create a canvas element for you that you
+  // can then insert into the DOM
+  canvas.replaceChildren(app.view);
 
-    resize();
+  viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: 100,
+    worldHeight: 100,
+    events: app.renderer.events,
+  })
+    .drag()
+    .pinch()
+    .wheel()
+    .decelerate();
 
-    // The application will create a canvas element for you that you
-    // can then insert into the DOM
-    canvas.replaceChildren(app.view);
+  app.stage.addChild(viewport);
 
-    app.stage.position.y = window.innerHeight / 2;
-    app.stage.position.x = window.innerWidth   / 2;
-    app.stage.scale.y = 2;
-    app.stage.scale.x = 2;
+  app.ticker.start();
 
-    return app;
-  }
+  app.stage.position.y = window.innerHeight / 2;
+  app.stage.position.x = window.innerWidth / 2;
+  app.stage.scale.y = 2;
+  app.stage.scale.x = 2;
 
-export async function addStarterObjects(app: PIXI.Application<HTMLCanvasElement>, net: Network) {
-  
-    // load the texture we need
-    const envelopeTexture = await PIXI.Assets.load(Envelope);
-    const routerTexture = await PIXI.Assets.load(Router);
+  return [app, viewport];
+}
 
+export async function addStarterObjects(
+  app: PIXI.Application<HTMLCanvasElement>,
+  net: Network
+) {
+  // load the texture we need
+  const envelopeTexture = await PIXI.Assets.load(Envelope);
+  const routerTexture = await PIXI.Assets.load(Router);
 
-    let text = new PIXI.HTMLText("Speed: " + speed + " visMode: " + visMode, {
-      fontSize: 20,
-      fill: "white",
-    });
+  let text = new PIXI.HTMLText("Speed: " + speed + " visMode: " + visMode, {
+    fontSize: 20,
+    fill: "white",
+  });
 
-    speed.subscribe((value) => {
-      text.text = "Speed: " + value;
-    });
+  viewport.addChild(text);
 
+  speed.subscribe((value) => {
+    text.text = "Speed: " + value;
+  });
 
-    net.get_graph()[0].forEach((v,k) => {
-      // This creates a texture from a 'bunny.png' image
-      const nodeSprite = new PIXI.Sprite(routerTexture);
-      nodeSprite.roundPixels = true;
-
-      // Setup the position of the bunny
-      if (v.nodeX) {
-        nodeSprite.x = v.nodeX;
-      } else {
-        nodeSprite.x = 0;
-      }
-      if (v.nodeY) {
-        nodeSprite.y = v.nodeY;
-      } else {
-        nodeSprite.y = 0;
-      }
-
-      app.stage.addChild(nodeSprite);
-    })
-
+  net.get_graph()[0].forEach((v, k) => {
     // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(envelopeTexture);
+    const nodeSprite = new PIXI.Sprite(routerTexture);
+    nodeSprite.roundPixels = true;
 
     // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+    if (v.nodeX) {
+      nodeSprite.x = v.nodeX;
+    } else {
+      nodeSprite.x = 0;
+    }
+    if (v.nodeY) {
+      nodeSprite.y = v.nodeY;
+    } else {
+      nodeSprite.y = 0;
+    }
 
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // Add the bunny to the scene we are building
-    app.stage.addChild(text);
-    app.stage.addChild(bunny);
-
-    // Listen for frame updates
-    app.ticker.add(() => {
-      // each frame we spin the bunny around a bit
-      bunny.rotation += 0.01;
-    });
+    viewport.addChild(nodeSprite);
+  });
 }
