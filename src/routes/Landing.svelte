5c34b07cc421cdb9052f789b0cf02fd7d13cd36a
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-11 03:23:48 -0600

    Added Landing styling

--- src/routes/Landing.svelte
+++ src/routes/Landing.svelte
@@ -3,17 +3,32 @@
   import "../assets/fonts/streamster/fonts.css";
 </script>
 
-<main>Welcome to Internet of Pings</main>
-
-<Link to="/sim?level=tutorial_1">Start</Link>
-<Link to="/sim?level=local">Use Local Storage</Link>
-<Link to="/sim?level=upload">Upload File</Link>
-
+<main>
+  <h1>Internet of Pings</h1>
+  <div id="options">
+    <div>
+      <Link to="/sim?level=tutorial_1">Start</Link>
+    </div>
+    <div>
+      <Link to="/sim?level=local">Use Local Storage</Link>
+    </div>
+    <div>
+      <Link to="/sim?level=upload">Upload File</Link>
+    </div>
+  </div>
+</main>
 
 <style>
-  main {
-    font-family: 'Streamster';
-    font-size: 80px;
+  h1 {
+    font-family: "Streamster";
+    font-size: 10rem;
     color: #f890e7;
+    margin-top: 0;
+    margin-bottom: 5rem;
+  }
+
+  #options {
+    display: inline-flex;
+    gap: 5ch;
   }
 </style>
