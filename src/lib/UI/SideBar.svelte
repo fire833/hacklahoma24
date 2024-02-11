6deecb4733495853e6142d2bfeb0ea65ba89fa10
Koy Wilson <koywilson@Koys-MacBook-Air.local>
2024-02-11 07:02:49 -0600

    Began working on tutorials

--- src/lib/UI/SideBar.svelte
+++ src/lib/UI/SideBar.svelte
@@ -1,18 +1,62 @@
 <script lang="ts">
+  import { writable } from "svelte/store";
   import AppButtons from "./AppButtons.svelte";
   import Interface from "./Interface.svelte";
+  import tutorialData1 from "./Tutorial_1.json";
+  import tutorialData2 from "./Tutorial_2.json";
+
+  let currentTut = tutorialData1;
+
+  enum SideBarModes {
+    Tutorial,
+    Node
+  }
+
+  let setting : SideBarModes = SideBarModes.Tutorial
+
+  let sideBarMode = writable<SideBarModes>(SideBarModes.Tutorial)
+  sideBarMode.subscribe((value) => {
+    setting = value;
+  });
+
+  function updateButton(mode: SideBarModes) {
+    sideBarMode.update(() => mode);
+  }
 
   export let open = false;
   let nodeName = "Test Node";
 
+
+  let counter = 1;
   let numberOfTimes = 3;
   let interfaces = Array.from(
     { length: numberOfTimes },
     (_, index) => index + 1
   );
+
+  function forwardClick() {
+    event?.preventDefault();
+    counter = counter + 1;
+    currentTut = eval(`tutorialData${counter}`);
+  }
+
+  function backwardClick() {
+    event?.preventDefault();
+    counter = counter - 1;
+    currentTut = eval(`tutorialData${counter}`);
+  }
 </script>
 
 <div id="bg" class={open ? "open" : "close"}>
+
+  {#each Object.values(SideBarModes).filter((value) => typeof value == "string") as mode} 
+    <button class:sunset={setting === SideBarModes[mode]} on:click={() => updateButton(SideBarModes[mode])}>
+      {mode}
+    </button>
+  {/each}
+
+  {#if setting === SideBarModes.Node}
+
   <h1>{nodeName}</h1>
   <h2>Interfaces</h2>
 
@@ -24,6 +68,32 @@
   <div id="apps-container">
     <AppButtons />
   </div>
+
+  {:else if setting === SideBarModes.Tutorial}
+    {#if currentTut}
+      <h1>{currentTut.title}</h1>
+
+      {#each currentTut.sections as section (section.title)}
+        <div>
+          <h2>{section.title}</h2>
+          {#each section.steps as step(step.text)}
+            <p>{step.text}</p>
+          {/each}
+        </div>
+      {/each}
+    {/if}
+
+
+    {#if counter >= 2}
+      <a href="sim?level=tutorial_{counter}"on:click={backwardClick}>Back</a>
+
+    {/if}
+
+    {#if counter <= 6} 
+      <a href="sim?level=tutorial_{counter}" on:click={forwardClick}>Next</a>
+
+    {/if}
+  {/if}
 </div>
 
 <style>
